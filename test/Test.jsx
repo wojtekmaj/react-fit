f8d638ce751223740b7981e63b0cdaf67e9d014b
Wojciech Maj <kontakt@wojtekmaj.pl>
2018-12-24 15:06:41 +0100

    Dynamically create colliding elements

--- test/Test.jsx
+++ test/Test.jsx
@@ -1,75 +1,109 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './Test.less';
 
 import ElementWithPopover from './ElementWithPopover';
 
-const Test = () => (
-  <div className="Test">
-    <header>
-      <h1>
-        react-fit test page
-      </h1>
-    </header>
-    <div className="Test__container">
-      <main className="Test__container__content">
-        <div
-          className="Test__elementWrapper"
-          style={{ top: 10, left: 10 }}
-        >
-          <ElementWithPopover
-            label="Bottom left - no collisions"
-          />
-        </div>
-        <div
-          className="Test__elementWrapper"
-          style={{ bottom: 10, right: 10 }}
-        >
-          <ElementWithPopover
-            label="Top right - no collisions"
-            invertAxis
-            invertSecondaryAxis
-          />
-        </div>
-        <div
-          className="Test__elementWrapper"
-          style={{ top: 62, right: 10 }}
-        >
-          <ElementWithPopover
-            label="Bottom left - horizontal collision"
-          />
-        </div>
-        <div
-          className="Test__elementWrapper"
-          style={{ bottom: 10, left: 150 }}
-        >
-          <ElementWithPopover
-            label="Bottom left - vertical collision"
-          />
-        </div>
-        <div
-          className="Test__elementWrapper"
-          style={{ bottom: 62, left: 10 }}
-        >
-          <ElementWithPopover
-            label="Top right - horizontal collision"
-            invertAxis
-            invertSecondaryAxis
-          />
-        </div>
-        <div
-          className="Test__elementWrapper"
-          style={{ top: 10, right: 150 }}
-        >
-          <ElementWithPopover
-            label="Top right - vertical collision"
-            invertAxis
-            invertSecondaryAxis
-          />
-        </div>
-      </main>
-    </div>
-  </div>
-);
+const BUTTON_WIDTH = 130;
+const BUTTON_HEIGHT = 42;
+const MARGIN = 10;
 
-export default Test;
+const BEGIN = { x: 'left', y: 'top' };
+const END = { x: 'right', y: 'bottom' };
+
+const corners = Array.from(new Array(4), (el, index) => ([index > 1, Boolean(index % 2)]));
+
+export default class Test extends Component {
+  state = {
+    mainAxis: 'y',
+  };
+
+  renderElement({
+    description, displayAbove, displayAlignRight, style,
+  }) {
+    const { mainAxis } = this.state;
+    const secondAxis = { x: 'y', y: 'x' }[mainAxis];
+    const MAIN_BEGIN = BEGIN[mainAxis];
+    const MAIN_END = END[mainAxis];
+    const SECOND_BEGIN = END[secondAxis];
+    const SECOND_END = BEGIN[secondAxis];
+    const first = displayAbove ? MAIN_BEGIN : MAIN_END;
+    const second = `align-${displayAlignRight ? SECOND_BEGIN : SECOND_END}`;
+
+    return (
+      <div
+        className="Test__elementWrapper"
+        style={style}
+      >
+        <ElementWithPopover
+          label={`${first} ${second} - ${description}`}
+          mainAxis={mainAxis}
+          invertAxis={displayAbove}
+          invertSecondaryAxis={displayAlignRight}
+        />
+      </div>
+    );
+  }
+
+  renderElements({ collideMainAxis, collideSecondaryAxis, description }) {
+    return corners.map(([invertAxis, invertSecondaryAxis]) => {
+      const displayAbove = collideSecondaryAxis ? !invertAxis : invertAxis;
+      const displayAlignRight = collideMainAxis ? !invertSecondaryAxis : invertSecondaryAxis;
+      const style = {};
+      style[invertAxis ? 'bottom' : 'top'] = MARGIN + (collideMainAxis ? MARGIN + BUTTON_HEIGHT : 0);
+      style[invertSecondaryAxis ? 'right' : 'left'] = MARGIN + (collideSecondaryAxis ? MARGIN + BUTTON_WIDTH : 0);
+
+      return this.renderElement({
+        description, displayAbove, displayAlignRight, style,
+      });
+    });
+  }
+
+  renderNoCollisionsElements() {
+    return this.renderElements({ description: 'no collisions' });
+  }
+
+  renderMainCollisionElements() {
+    const { mainAxis } = this.state;
+    return this.renderElements({
+      collideMainAxis: true,
+      description: `${mainAxis === 'y' ? 'horizontal' : 'vertical'} collision`,
+    });
+  }
+
+  renderSecondaryCollisionElements() {
+    const { mainAxis } = this.state;
+    return this.renderElements({
+      collideSecondaryAxis: true,
+      description: `${mainAxis === 'y' ? 'vertical' : 'horizontal'} collision`,
+    });
+  }
+
+  renderBothCollisionElements() {
+    return this.renderElements({
+      collideMainAxis: true,
+      collideSecondaryAxis: true,
+      description: 'both collisions',
+    });
+  }
+
+  render() {
+    return (
+      <div className="Test">
+        <header>
+          <h1>
+            react-fit test page
+          </h1>
+        </header>
+        <div className="Test__container">
+          <main className="Test__container__content">
+            {this.renderNoCollisionsElements()}
+            {this.renderMainCollisionElements()}
+            {this.renderSecondaryCollisionElements()}
+            {this.renderBothCollisionElements()}
+          </main>
+        </div>
+      </div>
+    );
+  }
+}
