daec3725ced0be98567f24b9564d77d2020be970
Wojciech Maj <kontakt@wojtekmaj.pl>
2020-12-04 12:22:10 +0100

    Update webpack-dev-server to 4.0.0-beta

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -74,9 +74,8 @@ module.exports = {
   },
   devServer: {
     compress: true,
-    historyApiFallback: true, // respond to 404s with index.html
+    historyApiFallback: true,
     host: '0.0.0.0',
-    hot: true, // enable HMR on the server
     port: 3000,
   },
 };
